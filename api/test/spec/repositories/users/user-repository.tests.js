9d1bdd1d4802abbb156382509ae14e4e4aad1874
Jeff Farley <jefffarley@Ghost.local>
2016-08-19 10:15:41 -0400

    Adding mocks to remove config dependencies. Closes #24

--- api/test/spec/repositories/users/user-repository.tests.js
+++ api/test/spec/repositories/users/user-repository.tests.js
@@ -1,25 +1,50 @@
+var proxyquire = require('proxyquire').noCallThru();
 
 describe('UserRepository Tests', function() {
+
+  // --------------------------------------------------------------------------
+  // Mocks
+
+  var dbMocks = {
+    pool: {
+      query: function(string, fn) {
+        var data = {
+          rows: [{
+            id: 1, 
+            name: "Jon Minter"
+        }]};
+
+        fn(null, data);
+      }
+    }
+  };
+
+  // --------------------------------------------------------------------------
+  // Setup
+
 
   var userRepository;
 
   beforeEach(function() {
-    userRepository = require('../../../../app/repositories/users/user-repository');
+    userRepository = proxyquire('../../../../app/repositories/users/user-repository', {
+      '../../config/db-config': dbMocks
+    });
   });
 
   describe('getUserData()', function() {
 
     it('should get a list of users', function(done) {
       userRepository.getUserList(function(users) {
-        //expect(users.length).to.be.greaterThan(0);
+        expect(users.length).to.be.greaterThan(0);
         done();
       });
     });
 
-    it('should get a single user', function(done) {
-      //var user = userRepository.getUser(1);
-      //expect(user).to.deep.equal({id: 1, name: "Jon Minter"});
+    xit('should get a single user', function(done) {
+      var user = userRepository.getUser(1);
+      expect(user).to.deep.equal({id: 1, name: "Jon Minter"});
       done();
+      pending('Not Currently Implemented');
     });
 
   });
