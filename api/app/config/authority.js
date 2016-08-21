6eef43c09a2623fa1d01f782a6f9b3c8a80c9c98
Trey White <trey@2codemonkeys.com>
2016-08-20 22:12:16 -0400

    Adding Basic Auth to all API calls

--- api/app/config/authority.js
+++ api/app/config/authority.js
@@ -1,22 +1,46 @@
+var auth = require('basic-auth');
+var md5 = require('md5');
 var api_config = require('./api-config');
 
 function Authority() {
 }
 
+const AUTH_MISSING_STRING = "You must provide an API Key and Authorization before accessing our service."
+const AUTH_ERROR_STRING   = "Authentication Failed";
+
 function checkForKey(req, res, next) {
   if( req.query.api_key == undefined ) {
-    res.status(401).send('You must provide an API Key before accessing our service.');
+    res.status(401).send(AUTH_MISSING_STRING);
   }
   else if(!api_config.checkForKey(req.query.api_key)) {
-    res.status(401).send('Unrecognized key');
+    res.status(401).send(AUTH_ERROR_STRING);
   }
   else {
     next();
   }
 }
 
+function authenticate(req, res, next) {
+  var userRepository_ = require('../repositories/users/user-repository');
+  var credentials = auth(req);
+  var apiRepresentation = false;
+
+  if(credentials != null) {
+    userRepository_.getUser(credentials.name, apiRepresentation, function(user) {
+      if(user != null && md5(credentials.pass.toLowerCase()) == user.password) {
+        next();
+      } else {
+        res.status(401).send(AUTH_ERROR_STRING);
+      }
+    });
+  } else {
+    res.status(401).send(AUTH_MISSING_STRING);
+  }
+}
+
 Authority.prototype = {
-  checkForKey: checkForKey
+  checkForKey: checkForKey,
+  authenticate: authenticate
 };
 
 var Authority = new Authority();
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 
 CREATE TABLE users (
   username  VARCHAR(32) PRIMARY KEY,
+  password  VARCHAR(32) not null,
   name      VARCHAR(64) not null,
   title     VARCHAR(64) not null,
   organization VARCHAR(64) not null,
