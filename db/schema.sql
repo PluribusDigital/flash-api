2c1c53816bce476e307fd4784d822ad00c421ec0
Trey White <trey@2codemonkeys.com>
2016-08-22 19:32:33 -0400

    Adding kudos schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,15 @@ CREATE TABLE users (
   organization VARCHAR(64) not null,
   department  VARCHAR(64) not null
 );
+
+
+CREATE TABLE kudos (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(32) not null,
+  full_text TEXT not null,
+  from_user_id INT not null,
+  to_user_id INT not null,
+  created_at DATE not null,
+  FOREIGN KEY (from_user_id) REFERENCES users (ID),
+  FOREIGN KEY (to_user_id) REFERENCES users (ID)
+);
