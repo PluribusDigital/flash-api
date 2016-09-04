262bb3cdc3c973e8ad99b7a48391c410dab2e1a0
Jeremy Altman <jeremy@540.co>
2016-09-03 22:26:37 -0400

    Rework controller code and update tests

--- api/test/spec/users/request.tests.js
+++ api/test/spec/users/request.tests.js
@@ -39,17 +39,33 @@ describe('User Request Tests', function() {
       .query({api_key: apiKey})
       .end(function(err, res) {
         expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body.data).to.be.a('array');
         done();
       });
     });
 
     it ('GET /v1/users/:username should return desired user', function (done) {
       chai.request(server)
-      .get('/v1/users/someuser')
+      .get('/v1/users/gwashington')
       .auth(user.username, user.password)
       .query({api_key: apiKey})
       .end(function(err, res) {
         expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body.data).to.be.a('object');
+        expect(res.body.data.username).to.equal('gwashington');
+        done();
+      });
+    });
+
+    it ('GET /v1/users/:username should return 404 when user is not found', function (done) {
+      chai.request(server)
+      .get('/v1/users/someuser')
+      .auth(user.username, user.password)
+      .query({api_key: apiKey})
+      .end(function(err, res) {
+        expect(res).to.have.status(404);
         done();
       });
     });
