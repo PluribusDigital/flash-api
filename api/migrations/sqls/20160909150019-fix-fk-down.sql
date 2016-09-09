6ac5782aa11a191d98aef77cd6c312795be96fd6
JeffreyMFarley <JeffreyMFarley@users.noreply.github.com>
2016-09-09 11:06:54 -0400

    Fix the Foreign Key contraints

---
+++ api/migrations/sqls/20160909150019-fix-fk-down.sql
@@ -0,0 +1,8 @@
+CREATE TABLE kudo (
+  id            INTEGER PRIMARY KEY DEFAULT nextval('kudo_serial'),
+  nominee       INTEGER REFERENCES kudo (id),
+  nominator     INTEGER REFERENCES kudo (id),
+  category      VARCHAR(40),
+  comment       VARCHAR(2000),
+  created_date  DATE
+);
